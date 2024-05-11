040a9d69d3550fb00595c840ba7ebce002452de4
KayJayo <115751956+KayJayo@users.noreply.github.com>
2024-05-11 07:30:36 -0700

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -10,23 +10,8 @@ winget install EclipseAdoptium.Temurin.8.JRE --accept-package-agreements --accep
 # Eclipse Temurin JRE 17
 winget install EclipseAdoptium.Temurin.17.JRE --accept-package-agreements --accept-source-agreements -h 
 
-# Heroic Games Launcher
-winget install HeroicGamesLauncher.HeroicGamesLauncher --accept-package-agreements --accept-source-agreements -h
-
 # Jellyfin Media Player
 winget install Jellyfin.JellyfinMediaPlayer --accept-package-agreements --accept-source-agreements -h
-
-# KDE Connect
-winget install KDE.KDEConnect --accept-package-agreements --accept-source-agreements -h
-
-# Krita
-winget install KDE.Krita --accept-package-agreements --accept-source-agreements -h
-
-# LibreOffice
-winget install TheDocumentFoundation.LibreOffice --accept-package-agreements --accept-source-agreements -h
-
-# LibreWolf
-winget install LibreWolf.LibreWolf --accept-package-agreements --accept-source-agreements -h
 
 # Microsoft Visual C++ Redistributable
 winget install Microsoft.VCRedist.2015+.x64 --accept-package-agreements --accept-source-agreements -h
@@ -40,14 +25,8 @@ winget install Mozilla.Thunderbird --accept-package-agreements --accept-source-a
 # Mp3tag
 winget install Mp3tag.Mp3tag --accept-package-agreements --accept-source-agreements -h
 
-# NVCleanstall
-winget install TechPowerUp.NVCleanstall --accept-package-agreements --accept-source-agreements -h
-
 # OBS Studio
 winget install OBSProject.OBSStudio --accept-package-agreements --accept-source-agreements -h
-
-# osu!
-winget install Peppy.Osu! --accept-package-agreements --accept-source-agreements -h
 
 # Parsec (shared install)
 winget install Parsec.Parsec --override "/silent /shared" --accept-package-agreements --accept-source-agreements -h
@@ -55,17 +34,14 @@ winget install Parsec.Parsec --override "/silent /shared" --accept-package-agree
 # Prism Launcher
 winget install PrismLauncher.PrismLauncher --accept-package-agreements --accept-source-agreements -h
 
-# PS Remote Play
-winget install PlayStation.PSRemotePlay --accept-package-agreements --accept-source-agreements -h
-
 # ShareX
 winget install ShareX.ShareX --accept-package-agreements --accept-source-agreements -h
 
-# Shotcut
-winget install Meltytech.Shotcut --accept-package-agreements --accept-source-agreements -h
+# Kdenlive
+winget install KDE.Kdenlive --accept-package-agreements --accept-source-agreements -h
 
-# Spotify
-winget install Spotify.Spotify --accept-package-agreements --accept-source-agreements -h
+# Cider
+winget install CiderCollective.Cider --accept-package-agreements --accept-source-agreements -h
 
 # Steam
 winget install Valve.Steam --accept-package-agreements --accept-source-agreements -h
@@ -73,20 +49,11 @@ winget install Valve.Steam --accept-package-agreements --accept-source-agreement
 # Sunshine
 winget install LizardByte.Sunshine --accept-package-agreements --accept-source-agreements -h
 
-# TegraRcmGUI
-winget install eliboa.TegraRcmGUI --accept-package-agreements --accept-source-agreements -h
-
 # TeraCopy
 choco install TeraCopy -y
 
-# TETR.IO
-winget install osk.tetr --accept-package-agreements --accept-source-agreements -h
-
 # WinSCP
 winget install WinSCP.WinSCP --accept-package-agreements --accept-source-agreements -h
-
-# Yubico Authenticator
-winget install Yubio.Authenticator --accept-package-agreements --accept-source-agreements -h
 
 # Pause for viewing
 pause
